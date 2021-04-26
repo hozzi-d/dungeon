cf8c385a8fc9f8df2d198afa1e816a8f55b0910e
hozzi-d <77388140+hozzi-d@users.noreply.github.com>
2021-04-26 08:49:24 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-function arthur2 () {
-	
-}
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
 	
 })
@@ -131,3 +128,25 @@ game.splash("START")
 pause(1000)
 scene.setBackgroundColor(1)
 tiles.setTilemap(tilemap`level3`)
+let arthur = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+arthur.setPosition(36, 36)
+controller.moveSprite(arthur)
+scene.cameraFollowSprite(arthur)
+tiles.placeOnRandomTile(arthur, assets.tile`myTile1`)
